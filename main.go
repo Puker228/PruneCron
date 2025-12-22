cb0ba8cffd67d3532e18fc59561360bc3ba5eb93
= <=>
2025-12-22 22:04:18 +0300

    prune containers

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/moby/moby/client"
+	"github.com/robfig/cron"
+)
 
 func main() {
-	fmt.Println("Hello prune")
+	c := cron.New()
+	c.AddFunc("@daily", func() {
+		ctx := context.Background()
+		apiClient, err := client.New(client.FromEnv)
+		if err != nil {
+			panic(err)
+		}
+		defer apiClient.Close()
+
+		_, error := apiClient.ContainerPrune(ctx, client.ContainerPruneOptions{})
+		if error != nil {
+			fmt.Printf("Error while prune containers: %v", error)
+		}
+	})
+	c.Start()
 }
